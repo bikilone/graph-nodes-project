15f51ffe5f525154cbe71d36945baddcffb0ae76
bikilone <milosbirovljevic@gmail.com>
2019-06-19 16:11:53 +0200

    Form created

--- src/app/graph-node/graph-node.module.ts
+++ src/app/graph-node/graph-node.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { GraphNodePage } from './graph-node.page';
+import { GraphNodePage } from "./graph-node.page";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: GraphNodePage
   }
 ];
@@ -17,7 +17,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
--- src/app/graph-node/graph-node.page.ts
+++ src/app/graph-node/graph-node.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { GraphNodesService } from "./graph-nodes.service";
 import { ActivatedRoute } from "@angular/router";
 import { GraphNode } from "./graph-node.model";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-graph-node",
@@ -11,20 +12,40 @@ import { GraphNode } from "./graph-node.model";
 export class GraphNodePage implements OnInit {
   selectedNode: GraphNode;
   childrenNodes: GraphNode[];
+  form: FormGroup;
   constructor(
     private graphNodesService: GraphNodesService,
     private route: ActivatedRoute
   ) {}
 
-  ionViewWillEnter() {
+  ngOnInit() {
+    this.form = new FormGroup({
+      parentObjects: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required]
+      }),
+      userId: new FormControl(null, {
+        updateOn: "blur"
+      }),
+      groupId: new FormControl(null, {
+        updateOn: "blur"
+      }),
+      roles: new FormControl(null, {
+        updateOn: "blur"
+      })
+    });
     this.route.paramMap.subscribe(paramMap => {
       if (!paramMap.has("id")) {
         return;
       }
       this.selectedNode = this.graphNodesService.getNode(paramMap.get("id"))[0];
+      console.log(this.selectedNode);
+
       this.childrenNodes = this.graphNodesService.findChildren(
         paramMap.get("id")
       );
     });
   }
+
+  onSubmit() {}
 }
