ab5b68b411f05ce67d0c8098466c5a87e47fb578
bikilone <milosbirovljevic@gmail.com>
2019-06-19 12:26:06 +0200

    Added GraphNodePage

--- src/app/graph-node/graph-node.page.spec.ts
+++ src/app/graph-node/graph-node.page.spec.ts
@@ -1,22 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomePage } from './home.page';
+import { GraphNodePage } from './graph-node.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('GraphNodePage', () => {
+  let component: GraphNodePage;
+  let fixture: ComponentFixture<GraphNodePage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ GraphNodePage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-      .compileComponents();
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(GraphNodePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/graph-node/graph-node.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-node',
+  templateUrl: './graph-node.page.html',
+  styleUrls: ['./graph-node.page.scss'],
+})
+export class GraphNodePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
